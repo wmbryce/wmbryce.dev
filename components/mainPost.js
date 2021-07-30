5db72ff6669e622edf72be035b7544713ded8c8b
Michael Bryce <michaelbryce@me.com>
2021-07-30 10:50:53 -0600

    Removed cover image

--- components/mainPost.js
+++ components/mainPost.js
@@ -1,13 +1,11 @@
-import CoverImage from '../components/coverImage';
+// import CoverImage from '../components/coverImage';
 import styles from '../styles/musings.module.css';
 import Link from 'next/link';
 
 export default function MainPost({title, coverImage, date, excerpt, author, slug}) {
   return (
     <div>
-      <div className={styles.imageContainer}>
-        <CoverImage title={title} src={coverImage} slug={slug} height={620} width={1240} />
-      </div>
+      <div className={styles.imageContainer}>{/* <CoverImage title={title} src={coverImage} slug={slug} height={620} width={1240} /> */}</div>
       <div className={styles.titleContainer}>
         <div>
           <h3 className={styles.articleTitle}>
@@ -15,8 +13,7 @@ export default function MainPost({title, coverImage, date, excerpt, author, slug
               <a className="hover:underline">{title}</a>
             </Link>
           </h3>
-          <div className={styles.subtitle}>
-          </div>
+          <div className={styles.subtitle}></div>
         </div>
         <div>
           <p className={styles.body}>{excerpt}</p>
